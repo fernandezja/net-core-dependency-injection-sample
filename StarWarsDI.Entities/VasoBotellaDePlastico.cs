ff2e64489459e01432dec4728b7f6e861033c7f3
fernandezja <fernandezja@gmail.com>
2021-06-02 22:07:05 -0300

    Update to NET5

---
+++ StarWarsDI.Entities/VasoBotellaDePlastico.cs
@@ -0,0 +1,29 @@
+﻿using StarWarsDI.Entities.Interfaces;
+using System;
+
+namespace StarWarsDI.Entities
+{
+    public class VasoBotellaDePlastico: IVasoBotellaPlastico, IVaso
+    {
+        public string Contenido { get; set; }
+
+        public bool EstaLleno { 
+            get {
+                return !string.IsNullOrEmpty(Contenido);
+            }
+        }
+
+        public bool MejorarBordesParaNoCortarse
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{{contenido:{Contenido}, estaLleno:{EstaLleno}, type=BotellaDePlastico}}";
+        }
+    }
+}
