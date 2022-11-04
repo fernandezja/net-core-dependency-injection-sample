213c3ef244c048ef28e07d09e685605cbf8d16d7
Jose A. Fernandez <fernandezja@gmail.com>
2022-11-04 16:01:05 -0300

    Chat example (IChatBusiness, IMensaje)

---
+++ StarWarsDI.Core.Data.Interfaces/IChatRepository.cs
@@ -0,0 +1,12 @@
+﻿using StarWarsDI.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StarWarsDI.Core.Data.Interfaces
+{
+    public interface IChatRepository
+    {
+        List<IMensaje> GetMensajesFromDb();
+    }
+}
